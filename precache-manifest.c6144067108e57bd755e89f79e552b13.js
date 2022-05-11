00acbbe8c9518eaf08d46335e8d1cc97a393049b
Ives <ivesmoraisbraga@gmail.com>
2022-05-11 15:36:20 -0300

    Updates

---
+++ precache-manifest.c6144067108e57bd755e89f79e552b13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca685fe7304b5fd9bb49a3314497c85",
+    "url": "/planet-research/index.html"
+  },
+  {
+    "revision": "66762ff95dd6d543d1b9",
+    "url": "/planet-research/static/css/2.a4e2f9ad.chunk.css"
+  },
+  {
+    "revision": "76392ca5065b97604cb9",
+    "url": "/planet-research/static/css/main.56328471.chunk.css"
+  },
+  {
+    "revision": "66762ff95dd6d543d1b9",
+    "url": "/planet-research/static/js/2.c41179d5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/planet-research/static/js/2.c41179d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76392ca5065b97604cb9",
+    "url": "/planet-research/static/js/main.bd35f5c2.chunk.js"
+  },
+  {
+    "revision": "c88429090f1b3164463a",
+    "url": "/planet-research/static/js/runtime-main.20a293f1.js"
+  }
+]);
